0edf71d3c48a2fcaf9b6abe33ca2619cbc24e0d3
manavdas1999 <61658813+manavdas1999@users.noreply.github.com>
2024-09-17 11:21:14 +0530

    Update first.java

--- Leetcode/LC-539-Minimum Time Difference/first.java
+++ Leetcode/LC-539-Minimum Time Difference/first.java
@@ -1,10 +1,14 @@
 
-// Idea:
+// IDEA:
 // 1. convert time to minutes for easy comparison
 // 2. store converted time points to an array (n2)
 // 3. sort the array - now we only have to check timeDiff in a single flow (nlogn)
 // 4. NOTE: Only catch is clock is round, i.e 22:00 is closer to 01:00 then 06:00 to 01:00;
 // hence to roundoff time, use 1440 + firstTime - lastTime (24:00 == 00:00 => 24*60+0 = 1440)
+
+// COMPLEXITY:
+// time : O(nLogn)
+// space: O(n)
 
 class Solution {
     public int findMinDifference(List<String> timePoints) {
